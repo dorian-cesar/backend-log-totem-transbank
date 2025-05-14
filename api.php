0858ec4e28f8100281fc2771938db91885e105d7
adolfosa <adolfo.sepulveda.astroza@gmail.com>
2025-05-14 14:45:05 -0400

    se corrigio orden campos nuevos

--- api.php
+++ api.php
@@ -203,7 +203,7 @@ if ($method == 'POST') {
             codigo_reserva, codigo_autorizacion, id_pos, numero_boleto, estado_boleto, 
             codigo_transaccion, estado_transaccion, numero_transaccion, 
             fecha_transaccion, hora_transaccion, total_transaccion, created_at
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())";
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())";
         
         $stmt = $pdo->prepare($sql);
         $stmt->execute([
